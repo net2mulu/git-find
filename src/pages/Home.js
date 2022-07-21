8c1ea116c6275da7fc048be2f35a2e2b7b46e003
net <nati2mulugeta@gmail.com>
2022-07-21 00:49:10 -0700

    user reult migrated to context

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+
+const Home = () => {
+  return (
+    <div>
+      <UserResults />
+    </div>
+  );
+};
+
+export default Home;
